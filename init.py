13c79900decbd3e447e1486d1191d879508690c3
mdecycu <mdecycuorg@gmail.com>
2025-10-29 14:23:00 +0800

    加入 w8 分組任務

--- init.py
+++ init.py
@@ -11,8 +11,7 @@ class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
     site_title = "cp2025"
-    #ip = "127.0.0.1"
-    ip = "2001:b011:d006:1349:c956:1cec:b46b:8ad1"
+    ip = "127.0.0.1"
     dynamic_port = 9442
     static_port = 8442
     def __init__(self):
